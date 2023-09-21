2aa3f6452ce9905eecdd3d9867fd16c5bf9902d0
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-09-21 23:05:00 +0300

    1.5

---
+++ src/js/function.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+//axios.defaults.headers.common["x-api-key"] = "39251396-18173d9ed82e61dff39932134";
+
+const urlApi = `https://pixabay.com/api/`;
+
+async function fetchPict(searchQuery, perPage, page){
+  const params = new URLSearchParams ({
+        q: searchQuery,
+        key: `39251396-18173d9ed82e61dff39932134`,
+        image_type: `photo`,
+        orientation: `horizontal`,
+        safesearch: `true`,
+        page: page,
+        per_page: perPage,
+  });
+  return response = await axios(`${urlApi}?${params}`)
+};
+
+// async function fetchPict(searchQuery, perPage, page){
+//   const params = new URLSearchParams ({
+//         q: searchQuery,
+//         key: `39251396-18173d9ed82e61dff39932134`,
+//         image_type: `photo`,
+//         orientation: `horizontal`,
+//         safesearch: `true`,
+//         page: page,
+//         per_page: perPage,
+//   });
+
+//   const response = await fetch(`${urlApi}?${params}`);
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return await response.json();
+// };
+
+// function fetchPict(searchQuery, perPage, page){
+//     const params = new URLSearchParams ({
+//         q: searchQuery,
+//         key: `39251396-18173d9ed82e61dff39932134`,
+//         image_type: `photo`,
+//         orientation: `horizontal`,
+//         safesearch: `true`,
+//         page: page,
+//         per_page: perPage,
+//     });
+//     return fetch (`${urlApi}?${params}`) 
+//    // resp = axios.get(`${urlApi}?${params}`)
+//     .then (response => {
+//         if (!response.ok) {
+//             throw new Error (response.status);
+//         }
+//         return response.json();
+// })
+// }
+
+/*
+webformatURL - посилання на маленьке зображення для списку карток.
+largeImageURL - посилання на велике зображення.
+tags - рядок з описом зображення. Підійде для атрибуту alt.
+likes - кількість лайків.
+views - кількість переглядів.
+comments - кількість коментарів.
+downloads - кількість завантажень.
+*/
+
+function createMarkup(arr) {
+    return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads})=>
+    `
+    <div class="photo-card">
+        <a class="photo-link" href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="200";
+          /></a>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: </b>${likes}
+            </p>
+            <p class="info-item">
+              <b>Views: </b>${views}
+            </p>
+            <p class="info-item">
+              <b>Comments: </b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads: </b>${downloads}
+            </p>
+          </div>
+      </div>
+    `
+    ).join(``);
+};
+
+export {createMarkup, fetchPict};
---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+console.log(`Hello, pict!`);
+
+import Notiflix, { Notify } from 'notiflix'; // Для відображення повідомлень користувачеві
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import {createMarkup, fetchPict} from '/src/js/function';
+
+const lightbox = new SimpleLightbox('.photo-card a', { 
+    captionsData: `alt`, // опис картинки
+    captionDelay: 250, // затримка показу в мс
+}); //оголошуємо змінну для підключення та оновлення бібліотеки
+
+const element = {
+    search: document.querySelector(`#search-form`),
+    btnSubmit: document.querySelector(`.js-submit`),
+    list: document.querySelector(`.gallery`),
+    loadMore: document.querySelector(`.js-load-more`),
+};
+
+let perPage = 40;
+let page;
+let totalPages;
+let searchQuery = ``;
+
+element.search.addEventListener(`submit`, handlerSearch);
+
+function handlerSearch(evt) {
+    evt.preventDefault();
+    element.loadMore.classList.replace(`load-more`, `load-more-hidden`);
+    element.list.innerHTML = ``;
+    page = 1;
+    
+    searchQuery = evt.currentTarget.elements.searchQuery.value;
+    console.log(searchQuery);
+
+    if (searchQuery === ""){ 
+        Notify.warning(`Attention! Field must be filled.`);} 
+    else {
+
+    fetchPict(searchQuery, perPage, page)
+    .then((response) => {
+        console.log(response.data);
+        //console.log(hits);
+        //console.log(data.totalHits);
+        totalPages = Math.ceil(response.data.totalHits/perPage);    
+        //console.log(totalPages);  
+
+        if(response.data.totalHits === 0) {
+            Notify.warning(`Sorry, there are no images matching your search query. Please try again.`);
+        } else {
+            Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+    
+        element.list.insertAdjacentHTML(`beforeend`, createMarkup(response.data.hits));
+        lightbox.refresh();
+        
+        if (page < totalPages){
+            element.loadMore.classList.replace(`load-more-hidden`, `load-more`)
+        }}
+    })
+    .catch((_err) => {
+        element.loadMore.classList.replace("load-more", "load-more-hidden");
+    })};
+};
+
+element.loadMore.addEventListener(`click`, handlerLoadMore);
+
+function handlerLoadMore(){
+    page +=1;
+    //console.log(page)
+    fetchPict(searchQuery, perPage, page)
+    .then((response) => {
+        element.list.insertAdjacentHTML(`beforeend`, createMarkup(response.data.hits));
+        lightbox.refresh();
+
+        if (page >= totalPages){
+            Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
+            element.loadMore.classList.replace(`load-more`, `load-more-hidden`);
+            }
+        else
+        {element.loadMore.classList.replace(`load-more-hidden`, `load-more`)}
+    })
+    .catch((_err) => {
+        element.loadMore.classList.replace("load-more", "load-more-hidden");
+    })
+}
